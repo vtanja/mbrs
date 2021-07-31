a7b36d7d76749c565d23cbd6c8a36ef87353b8a9
Tanja <tanja.vukmirovic8@gmail.com>
2021-07-31 11:15:37 +0200

    Added DTOGenerator class

---
+++ PluginDevelopment/src/myplugin/generator/DTOGenerator.java
@@ -0,0 +1,9 @@
+package myplugin.generator;
+
+import myplugin.generator.options.GeneratorOptions;
+
+public class DTOGenerator extends BasicGenerator {
+	public DTOGenerator(GeneratorOptions generatorOptions) {
+		super(generatorOptions);
+	}
+}
