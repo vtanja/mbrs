c4737bdd89495508f74fc93390515f56f8447351
Tanja <tanja.vukmirovic8@gmail.com>
2021-08-31 11:25:23 +0200

    Added ControllerGenerator class

---
+++ PluginDevelopment/src/myplugin/generator/ControllerGenerator.java
@@ -0,0 +1,55 @@
+package myplugin.generator;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+import freemarker.template.TemplateException;
+import myplugin.generator.fmmodel.FMClass;
+import myplugin.generator.fmmodel.FMIdentityProperty;
+import myplugin.generator.fmmodel.FMLinkedProperty;
+import myplugin.generator.fmmodel.FMModel;
+import myplugin.generator.fmmodel.FMPersistentProperty;
+import myplugin.generator.fmmodel.FMProperty;
+import myplugin.generator.options.GeneratorOptions;
+
+public class ControllerGenerator extends BasicGenerator {
+	
+	public ControllerGenerator(GeneratorOptions generatorOptions) {
+		super(generatorOptions);
+	}
+	
+	public void generate() {
+		try {
+			super.generate();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+		
+		List<FMClass> classes = FMModel.getInstance().getClasses();
+		for (int i = 0; i < classes.size(); i++) {
+			FMClass cl = classes.get(i);
+			Writer out;
+			Map<String, Object> context = new HashMap<String, Object>();
+			try {
+				out = getWriter(cl.getName(), cl.getTypePackage());
+				if (out != null) {
+					context.clear();
+					context.put("name", cl.getName());
+															
+					getTemplate().process(context, out);
+					out.flush();
+				}
+			} catch (TemplateException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage());
+			} catch (IOException e) {
+				JOptionPane.showMessageDialog(null, e.getMessage());
+			}
+		}
+	}
+}
