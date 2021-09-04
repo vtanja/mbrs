d70a17b875a126854b8127d3b926119caad13f5c
Tanja <tanja.vukmirovic8@gmail.com>
2021-09-04 16:26:04 +0200

    Updated GetGenerator method to accept as parameter typeMapping

--- PluginDevelopment/src/myplugin/generator/GeneratorFactory.java
+++ PluginDevelopment/src/myplugin/generator/GeneratorFactory.java
@@ -10,18 +10,18 @@ public class GeneratorFactory {
 	public static BasicGenerator GetGenerator(Map.Entry<String, GeneratorOptions> entry, List<TypeMapping> typeMappings) {
 		
 		if(entry.getKey().equals("DTOGenerator")) {
-			return new DTOGenerator(entry.getValue());
+			return new DTOGenerator(entry.getValue(), typeMappings);
 		}
 		else if(entry.getKey().equals("FrontDTOGenerator")) {
 			return new FrontDTOGenerator(entry.getValue(), typeMappings);
 		}
 		
 		if(entry.getKey().equals("ControllerGenerator")) {
-			return new ControllerGenerator(entry.getValue());
+			return new ControllerGenerator(entry.getValue(), typeMappings);
 		}
 		
 		if(entry.getKey().equals("DTODetailGenerator")) {
-			return new DTODetailGenerator(entry.getValue());
+			return new DTODetailGenerator(entry.getValue(), typeMappings);
 		}
 		
 		return null;
