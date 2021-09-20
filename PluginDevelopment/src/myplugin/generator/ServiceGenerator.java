373a546dfbfa6836a89746b9b877ce554dfd1e7d
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-09-20 17:53:00 +0200

    Remove lowercase from class name for filename

--- PluginDevelopment/src/myplugin/generator/ServiceGenerator.java
+++ PluginDevelopment/src/myplugin/generator/ServiceGenerator.java
@@ -39,7 +39,7 @@ public class ServiceGenerator extends BasicGenerator {
 			Writer out;
 			Map<String, Object> context = new HashMap<String, Object>();
 			try {
-				out = getWriter(cl.getName().toLowerCase(), cl.getTypePackage());
+				out = getWriter(cl.getName(), cl.getTypePackage());
 				if (out != null) {
 					context.clear();
 					context.put("class", cl);
