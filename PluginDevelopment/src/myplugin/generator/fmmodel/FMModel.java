0a2fba4c70d757a5391e055ffbb0175468f81705
Tanja <tanja.vukmirovic8@gmail.com>
2021-10-10 16:22:58 +0200

    Added information about id names

--- PluginDevelopment/src/myplugin/generator/fmmodel/FMModel.java
+++ PluginDevelopment/src/myplugin/generator/fmmodel/FMModel.java
@@ -1,7 +1,10 @@
 package myplugin.generator.fmmodel;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 /** FMModel: Singleton class. This is intermediate data structure that keeps metadata
  * extracted from MagicDraw model. Data structure should be optimized for code generation
@@ -14,6 +17,16 @@ public class FMModel {
 	private List<FMEnumeration> enumerations = new ArrayList<FMEnumeration>();
 	private FMApplication application;
 	private List<FMComponent> components = new ArrayList<FMComponent>();
+	private Map<String, String> idNames = new HashMap<String, String>();
+	
+	public Map<String, String> getIdNames() {
+		return idNames;
+	}
+	
+	public void AddIdName(String component, String idName){
+		idNames.put(component, idName);
+	}
+
 	//....
 	/** @ToDo: Add lists of other elements, if needed */
 	private FMModel() {
