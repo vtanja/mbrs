2ca8c4febed09b89a2ec178d68b460422b95d1b9
nikolina-sarenac <nina.sarenac@hotmail.com>
2021-10-16 13:51:50 +0200

    ts generator class

---
+++ PluginDevelopment/src/myplugin/generator/FormComponentTsGenerator.java
@@ -0,0 +1,62 @@
+package myplugin.generator;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.swing.JOptionPane;
+
+import freemarker.template.TemplateException;
+import myplugin.generator.fmmodel.FMClass;
+import myplugin.generator.fmmodel.FMComponent;
+import myplugin.generator.fmmodel.FMField;
+import myplugin.generator.fmmodel.FMIdentityProperty;
+import myplugin.generator.fmmodel.FMLinkedProperty;
+import myplugin.generator.fmmodel.FMModel;
+import myplugin.generator.fmmodel.FMPersistentProperty;
+import myplugin.generator.fmmodel.FMProperty;
+import myplugin.generator.options.GeneratorOptions;
+import myplugin.generator.options.TypeMapping;
+
+public class FormComponentTsGenerator extends BasicGenerator{
+	public FormComponentTsGenerator(GeneratorOptions generatorOptions, List<TypeMapping> typeMappings) {
+		super(generatorOptions, typeMappings);
+	}
+	
+	public void generate() {
+		try {
+			super.generate();
+		} catch (IOException e) {
+			JOptionPane.showMessageDialog(null, e.getMessage());
+		}
+		
+		List<FMComponent> components = FMModel.getInstance().getComponents();
+		for (int i = 0; i < components.size(); i++) {
+			FMComponent component = components.get(i);
+			if(component.isUpdate() || component.isCreate()) {
+				Writer out;
+				Map<String, Object> context = new HashMap<String, Object>();
+				try {
+					out = getWriter(component.getName().substring(0, 1).toLowerCase() + component.getName().substring(1), "");
+					if (out != null) {
+						
+						context.put("id", FMModel.getInstance().getIdNames().get(component.getName())!=null?FMModel.getInstance().getIdNames().get(component.getName()):"");
+						context.put("component", component);
+						context.put("entity_name", component.getName());
+
+						context.put("fields", component.getFields());
+						getTemplate().process(context, out);
+						out.flush();
+					}					
+				} catch (TemplateException e) {
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				} catch (IOException e) {
+					JOptionPane.showMessageDialog(null, e.getMessage());
+				}
+			}
+		}
+	}
+}
