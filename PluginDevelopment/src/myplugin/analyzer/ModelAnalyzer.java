f9f09830a81b08ec35f5e99f235b697b9d73df07
Tanja <tanja.vukmirovic8@gmail.com>
2021-10-27 16:51:20 +0200

    Fixed loop count

--- PluginDevelopment/src/myplugin/analyzer/ModelAnalyzer.java
+++ PluginDevelopment/src/myplugin/analyzer/ModelAnalyzer.java
@@ -38,6 +38,7 @@ import com.nomagic.uml2.ext.jmi.helpers.StereotypesHelper;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Element;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.EnumerationLiteral;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.NamedElement;
+import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Operation;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Package;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Class;
 import com.nomagic.uml2.ext.magicdraw.classes.mdkernel.Enumeration;
@@ -289,7 +290,7 @@ public class ModelAnalyzer {
 				fmClass.addImportedPackage(fmType);
 			}
 		}
-		
+				
 		return fmClass;
 	}
 	
@@ -592,7 +593,7 @@ public class ModelAnalyzer {
 	private FMEnumeration getEnumerationData(Enumeration enumeration, String packageName) throws AnalyzeException {
 		FMEnumeration fmEnum = new FMEnumeration(enumeration.getName(), packageName);
 		List<EnumerationLiteral> list = enumeration.getOwnedLiteral();
-		for (int i = 0; i < list.size() - 1; i++) {
+		for (int i = 0; i < list.size(); i++) {
 			EnumerationLiteral literal = list.get(i);
 			if (literal.getName() == null)  
 				throw new AnalyzeException("Items of the enumeration " + enumeration.getName() +
